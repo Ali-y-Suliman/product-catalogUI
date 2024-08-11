9a4c5363058acce6d08ede7ad843878142ad42fc
Ali Suliman <a.suliman@oxinus.io>
2024-08-11 23:30:26 +0400

    fix test step in deployment

--- src/components/Categories/CategoriesList.tsx
+++ src/components/Categories/CategoriesList.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useCallback, useEffect } from "react";
 import { useAlert } from "../../contexts/AlertContext";
 import { useApi } from "../../hooks/useApi";
--- src/components/Common/CreateModal.tsx
+++ src/components/Common/CreateModal.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ObjectType } from "typescript";
 
 interface ObjectModelProps {
